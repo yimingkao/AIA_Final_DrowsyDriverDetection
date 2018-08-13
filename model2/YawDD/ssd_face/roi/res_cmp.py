22b5fead004e33d8fd07cf3ab4ab71c4d8f70f08
Yi-Ming Kao <yimingkao@gmail.com>
2018-08-13 21:23:12 +0800

    Modify res_cmp.py to compare 3 different features.

--- model2/YawDD/ssd_face/roi/res_cmp.py
+++ model2/YawDD/ssd_face/roi/res_cmp.py
@@ -2,6 +2,7 @@ import os
 import re
 import pickle
 import numpy as np
+import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -24,6 +25,7 @@ for set_name in ['yawn_valid', 'yawn_test']:
         with open('lstm_'+extractor+'_'+str(n_features)+'_'+set_name+'.pickle', 'rb') as f:
             data.append(pickle.load(f))
     
+    history = []
     for i in range(len(data[0])):
         fname = data[0][i][2]
         mark_path = '../../../../YawDD/'
@@ -41,9 +43,12 @@ for set_name in ['yawn_valid', 'yawn_test']:
         axisx = [i for i in range(len(y))]
         plt.figure(figsize=(16,7))
         plt.plot(axisx, y, label='golden')
+        entry = [fname]
         for j in range(len(features)):
             label = str(features[j])+','+'%.2f'%data[j][i][1][0]
             plt.plot(axisx, data[j][i][0], label=label)
+            entry.extend(data[j][i][1][0])
+        history.append(entry)
         plt.legend()
         plt.tight_layout()
         plt.ylabel('degree')
@@ -51,5 +56,10 @@ for set_name in ['yawn_valid', 'yawn_test']:
         plt.savefig(dst_path+fname.replace('.avi', '.png'))
         plt.clf()
         plt.close()
-        print('%d/%d: '%(i, len(data)), fname.replace('.avi', '.png') + " saved!")
-    
+        print('%d/%d: '%(i, len(data[0])), fname.replace('.avi', '.png') + " saved!")
+    #print(history)
+    col_name = ['name']
+    for j in range(len(features)):
+        col_name.append(str(features[j]))
+    loss = pd.DataFrame(history, columns=col_name)
+    loss.to_csv(set_name+'_cmp.csv', index=False)
