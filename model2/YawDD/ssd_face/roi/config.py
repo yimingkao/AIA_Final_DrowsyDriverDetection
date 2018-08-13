73d238dfe9fb8c2a8859b69875ff80fd8515a02a
Yi-Ming Kao <yimingkao@gmail.com>
2018-08-13 15:43:26 +0800

    Add MobileNet since server doesn't support MobileNetV2

--- model2/YawDD/ssd_face/roi/config.py
+++ model2/YawDD/ssd_face/roi/config.py
@@ -1,11 +1,11 @@
 
 from dense121_fea_extract import Dense121FeatureExtract
-from mobilenetv2_fea_extract import MobileNetV2FeatureExtract
+from mobilenet_fea_extract import MobileNetFeatureExtract
 
 extractor = 'dense121'
 featureExtracter = Dense121FeatureExtract(N_FEATURES)
-#extractor = 'mobilenetv2'
-#featureExtracter = MobileNetV2FeatureExtract(N_FEATURES)
+#extractor = 'mobilenet'
+#featureExtracter = MobileNetFeatureExtract(N_FEATURES)
 
 N_FEATURES = 2048
 video_path = '/projectdata/driver/YawDD/'
