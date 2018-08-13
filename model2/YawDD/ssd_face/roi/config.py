7681ecc967acca3403bb27015f0dad1318e68720
Yi-Ming Kao <yimingkao@gmail.com>
2018-08-13 14:39:42 +0800

    Add MobileNetV2.

--- model2/YawDD/ssd_face/roi/config.py
+++ model2/YawDD/ssd_face/roi/config.py
@@ -1,8 +1,13 @@
 
 from dense121_fea_extract import Dense121FeatureExtract
+from mobilenetv2_fea_extract import MobileNetV2FeatureExtract
 
-N_FEATURES = 2048
 extractor = 'dense121'
 featureExtracter = Dense121FeatureExtract(N_FEATURES)
+#extractor = 'mobilenetv2'
+#featureExtracter = MobileNetV2FeatureExtract(N_FEATURES)
+
+N_FEATURES = 2048
 video_path = '/projectdata/driver/YawDD/'
 #video_path = '../../../../../YawDD/'
+
