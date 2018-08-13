5a2b6ea6d7a4bcab75ccc6cc4eb395a052a2fffa
at072210 <at072210@local>
2018-08-13 09:52:01 +0000

    Fix N_FEATURES is not defined issue

--- model2/YawDD/ssd_face/roi/config.py
+++ model2/YawDD/ssd_face/roi/config.py
@@ -2,12 +2,12 @@
 from dense121_fea_extract import Dense121FeatureExtract
 from mobilenet_fea_extract import MobileNetFeatureExtract
 
+N_FEATURES = 1024
+video_path = '/projectdata/driver/YawDD/'
+#video_path = '../../../../../YawDD/'
+
 extractor = 'dense121'
 featureExtracter = Dense121FeatureExtract(N_FEATURES)
 #extractor = 'mobilenet'
 #featureExtracter = MobileNetFeatureExtract(N_FEATURES)
-
-N_FEATURES = 2048
-video_path = '/projectdata/driver/YawDD/'
-#video_path = '../../../../../YawDD/'
 
