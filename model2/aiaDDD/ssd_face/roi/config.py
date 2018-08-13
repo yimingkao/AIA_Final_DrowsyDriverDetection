e3616c67c789944467a5e422a64dda092229a5f8
Yi-Ming Kao <yimingkao@gmail.com>
2018-08-13 18:43:14 +0800

    Add aiaDDD config

--- model2/aiaDDD/ssd_face/roi/config.py
+++ model2/aiaDDD/ssd_face/roi/config.py
@@ -3,8 +3,7 @@ from dense121_fea_extract import Dense121FeatureExtract
 from mobilenet_fea_extract import MobileNetFeatureExtract
 
 N_FEATURES = 1024
-video_path = '/projectdata/driver/YawDD/'
-#video_path = '../../../../../YawDD/'
+video_path = '/home/jovyan/at072-group05/aiaDDD/videos/'
 
 extractor = 'dense121'
 featureExtracter = Dense121FeatureExtract(N_FEATURES)
